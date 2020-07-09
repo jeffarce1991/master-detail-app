26dcba6c007846559434c049ac00ae582649fae9
jeff <jeffandrew.s.arce@gmail.com>
2020-07-09 20:12:03 +0800

    Add DefaultMediaRemoteLoader.

---
+++ app/src/main/java/com/jeff/master/webservices/usecase/loader/DefaultMediaRemoteLoader.kt
@@ -0,0 +1,22 @@
+package com.jeff.master.webservices.usecase.loader
+
+import com.jeff.master.webservices.api.ApiFactory
+import com.jeff.master.webservices.api.photos.ItunesApi
+import com.jeff.master.webservices.dto.MediaResultsDto
+import com.jeff.master.webservices.transformer.NullResultTransformer
+import com.jeff.master.webservices.transformer.ResponseCodeNot200SingleTransformer
+import io.reactivex.Single
+import javax.inject.Inject
+
+class DefaultMediaRemoteLoader @Inject
+constructor(private val apiFactory: ApiFactory): MediaRemoteLoader {
+
+    override fun loadAll(): Single<MediaResultsDto> {
+        return apiFactory.create(ItunesApi::class.java)
+            .flatMap { it.loadTracks() }
+            .compose(ResponseCodeNot200SingleTransformer())
+            .compose(NullResultTransformer())
+            .flatMap { response ->
+                Single.just(response.body()!!) }
+    }
+}
