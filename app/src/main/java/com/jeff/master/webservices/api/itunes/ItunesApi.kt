aba85bdc43e2ece022c119aa452583a52a14c9c3
jeff <jeffandrew.s.arce@gmail.com>
2020-07-10 02:14:29 +0800

    Add loadById on ItunesApi.

--- app/src/main/java/com/jeff/master/webservices/api/itunes/ItunesApi.kt
+++ app/src/main/java/com/jeff/master/webservices/api/itunes/ItunesApi.kt
@@ -1,17 +1,17 @@
-package com.jeff.master.webservices.api.photos
+package com.jeff.master.webservices.api.itunes
 
-import com.jeff.master.webservices.dto.PhotoDto
 import com.jeff.master.webservices.dto.MediaResultsDto
 import io.reactivex.Single
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface ItunesApi {
 
     @GET("search?term=star&amp;country=au&amp;media=movie&amp;all")
     fun loadTracks(): Single<Response<MediaResultsDto>>
 
-    @GET("photos/{id}")
-    fun loadPhotoById(@Path("id") id: Int): Single<Response<PhotoDto>>
+    @GET("lookup")
+    fun loadById(@Query("id") id: Int): Single<Response<MediaResultsDto>>
 }
