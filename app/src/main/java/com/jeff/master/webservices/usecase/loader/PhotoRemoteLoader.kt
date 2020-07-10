9678df84d889d9475f0c8f3099168c7186404b69
jeff <jeffandrew.s.arce@gmail.com>
2020-07-11 00:24:09 +0800

    Removed unused classes and resources.

--- app/src/main/java/com/jeff/master/webservices/usecase/loader/PhotoRemoteLoader.kt
+++
@@ -1,9 +0,0 @@
-package com.jeff.master.webservices.usecase.loader
-
-import com.jeff.master.webservices.dto.PhotoDto
-import io.reactivex.Single
-
-interface PhotoRemoteLoader {
-
-    fun loadAll(): Single<List<PhotoDto>>
-}
