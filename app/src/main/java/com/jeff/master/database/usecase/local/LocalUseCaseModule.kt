54cfe7a39812e8987393c7b36758b0a902421da9
jeff <jeffandrew.s.arce@gmail.com>
2020-07-10 21:37:11 +0800

    Binds MediaLocalSaver and MediaLocalLoader.

--- app/src/main/java/com/jeff/master/database/usecase/local/LocalUseCaseModule.kt
+++ app/src/main/java/com/jeff/master/database/usecase/local/LocalUseCaseModule.kt
@@ -1,17 +1,28 @@
 package com.jeff.master.database.usecase.local
 
+import com.jeff.master.database.usecase.local.loader.DefaultMediaLocalLoader
 import com.jeff.master.database.usecase.local.loader.DefaultPhotoLocalLoader
+import com.jeff.master.database.usecase.local.loader.MediaLocalLoader
 import com.jeff.master.database.usecase.local.loader.PhotoLocalLoader
+import com.jeff.master.database.usecase.local.saver.DefaultMediaLocalSaver
 import com.jeff.master.database.usecase.local.saver.DefaultPhotoLocalSaver
+import com.jeff.master.database.usecase.local.saver.MediaLocalSaver
 import com.jeff.master.database.usecase.local.saver.PhotoLocalSaver
 import dagger.Binds
 import dagger.Module
 
 @Module
 interface LocalUseCaseModule {
+
     @Binds
     fun bindPhotoLocalLoader(implementation: DefaultPhotoLocalLoader): PhotoLocalLoader
 
     @Binds
     fun bindPhotoLocalSaver(implementation: DefaultPhotoLocalSaver): PhotoLocalSaver
+
+    @Binds
+    fun bindMediaLocalLoader(implementation: DefaultMediaLocalLoader): MediaLocalLoader
+
+    @Binds
+    fun bindMediaLocalSaver(implementation: DefaultMediaLocalSaver): MediaLocalSaver
 }
