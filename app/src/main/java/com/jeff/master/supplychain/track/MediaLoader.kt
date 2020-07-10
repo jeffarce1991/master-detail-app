b7485328e5c599f7638d075bd27a57f5ee79e395
jeff <jeffandrew.s.arce@gmail.com>
2020-07-11 00:01:54 +0800

    Update MediaLoader Interface functions.

--- app/src/main/java/com/jeff/master/supplychain/track/MediaLoader.kt
+++ app/src/main/java/com/jeff/master/supplychain/track/MediaLoader.kt
@@ -4,7 +4,12 @@ import com.jeff.master.database.local.Media
 import io.reactivex.Single
 
 interface MediaLoader {
-
     fun loadAll(): Single<List<Media>>
-    fun loadById(id: Int): Single<Media>
+
+    fun loadAllRemotely(): Single<List<Media>>
+    fun loadByIdRemotely(id: Int): Single<Media>
+
+
+    fun loadAllLocally(): Single<List<Media>>
+    fun loadByIdLocally(id: Int): Single<Media>
 }
